2e668957b915fd6b4e397b2a6e027d73cee5774a
buddygn <buddygn@gmail.com>
2023-07-07 18:35:53 -0300

    feat: Implementing user_register

--- src/data/use_cases/user_finder_test.py
+++ src/data/use_cases/user_finder_test.py
@@ -1,6 +1,60 @@
-from src.infra.db.repositories.users_repository import UsersRepository
+from typing import List
+from src.domain.models.users import Users
+from src.infra.db.tests.users_repository import UsersRepositorySpy
 from .user_finder import UserFinder
 
 def test_find():
-    repo = UsersRepository()
+    first_name = 'meunome'
+
+    repo = UsersRepositorySpy()
     user_finder = UserFinder(repo)
+
+    response = user_finder.find(first_name)
+
+    assert  repo.select_user_attributes['first_name'] == first_name
+    assert  response['type'] == 'Users'
+    assert  response['count'] == len(response['attributes'])
+    assert  response['attributes']
+
+
+def test_find_error_in_valid_name():
+    first_name = 'meunome123'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == 'Nome inválido para a busca'
+
+
+def test_find_error_in_long_name():
+    first_name = 'meunomejacarebuscadorerradoeqweqweqweqw'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == 'Nome muito grande para busca'
+
+
+def test_find_error_user_not_found():
+    class UsersRepositoryError(UsersRepositorySpy):
+        def select_user(self, first_name: str) -> List[Users]:
+            return []
+
+    first_name = 'menuNome'
+
+    repo = UsersRepositoryError()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == 'Usuário não encontrado'
