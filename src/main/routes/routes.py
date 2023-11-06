0e4ab88ff825f22d76f91b5748829cce584dfd3b
buddygn <buddygn@gmail.com>
2023-11-06 16:01:35 -0300

    feat: Implementing handler errors

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -6,16 +6,31 @@ from src.main.adapters.request_adapter import request_adapter
 from src.main.composers.user_finder_composer import user_finder_composer
 from src.main.composers.user_register_composer import user_register_composer
 
+# Error handler
+from src.errors.error_handler import handle_error
+
 user_routes_bp = Blueprint('user_routes',  __name__)
 
 
 @user_routes_bp.route('/user/find', methods=['GET'])
 def find_user():
-    http_response = request_adapter(request, user_finder_composer())
+    http_response =  None
+
+    try:
+        http_response = request_adapter(request, user_finder_composer())
+    except Exception as exception:
+        http_response = handle_error(exception)
+
     return jsonify(http_response.body), http_response.status_code
 
 
 @user_routes_bp.route('/user', methods=['POST'])
 def register_user():
-    http_response = request_adapter(request, user_register_composer())
+    http_response =  None
+
+    try:
+        http_response = request_adapter(request, user_register_composer())
+    except Exception as exception:
+        http_response = handle_error(exception)
+
     return jsonify(http_response.body), http_response.status_code
